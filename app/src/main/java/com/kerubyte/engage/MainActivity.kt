4adadf1d35171c0eaea05d68253fd57180a9760d
dawidtyburek <kerubyte@gmail.com>
2023-02-08 22:36:00 +0100

    Initial commit

---
+++ app/src/main/java/com/kerubyte/engage/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.kerubyte.engage
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.kerubyte.engage.ui.theme.EngageTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EngageTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                }
+            }
+        }
+    }
+}
